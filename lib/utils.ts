41806c654e86349549367a7ccb6dda4b616e305c
edithluv <kay_oloko@yahoo.co.uk>
2025-07-31 18:51:01 +0100

    Fix corrupted spread operators (@. to ...)

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,6 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
-export function cn(p0: string, className: string | undefined, @.inputs
-  : ClassValue[]) {
+export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
