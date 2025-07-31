10d2bd3dc547f622d07191c434e295a1cd0bfa2c
edithluv <kay_oloko@yahoo.co.uk>
2025-07-31 19:09:19 +0100

    Fix corrupted spread operators (@. to ...)

--- components/settings-panel.tsx
+++ components/settings-panel.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { Label } from "../components/ui/label"
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../components/ui/select"
 import { Switch } from "../components/ui/switch"
 import { Separator } from "../components/ui/separator"
 
